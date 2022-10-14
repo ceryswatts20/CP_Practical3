0f4cf7c169d9e10f9bf687d450761b2b81a3975e
Cerys Watts <ceryswatts942@gmail.com>
2022-10-14 13:02:19 +0100

    Added comment to explain fetch_and_add method

--- src/LockMutex.java
+++ src/LockMutex.java
@@ -34,6 +34,15 @@ public class LockMutex {
         lock.unlock();
     }
 
+    // In principle: creates a queue
+    // Which ever threads executes this first will enter their critical section 1st
+    // E.g thread 2: executes fetch_and_add
+    //     thread 1: executes fetch_and_add
+    //     thread 2: executes critical section
+    //     thread 2: executes fetch_and_add
+    //     thread 1: executes critical section
+    //     thread 2: executes critical section
+
     private void fetch_and_add(AtomicInteger var, int value, AtomicInteger old) {
         old.set(var.get());
         var.getAndAdd(value);
